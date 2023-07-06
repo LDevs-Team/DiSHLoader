b69b14dc268b8b41d56884e6a365033a5e5bc2dc
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2023-07-06 17:28:10 +0200

    chore!: made directory structure conventional

--- DiSHLoader/Program.cs
+++ DiSHLoader/Program.cs
@@ -4,34 +4,26 @@ using System.Runtime.CompilerServices;
 using dotenv.net;
 using Microsoft.Win32;
 
-// Token: 0x02000002 RID: 2
-[CompilerGenerated]
-internal class Program
+
+
+string AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+string DefaultVersion = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\LDevs\\DiSHLoader\\", "DefaultVersion", null).ToString();
+string EnvFilePath = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\LDevs\\DiSHLoader\\", "EnvPath", AppDataPath + "\\LDevs\\DiSHLoader\\.env").ToString();
+string VersionsPath = Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\LDevs\\DiSHLoader\\", "EnvPath", AppDataPath + "\\LDevs\\DiSHLoader\\versions\\").ToString();
+Console.WriteLine("Default version -> " + DefaultVersion);
+Console.WriteLine("Env file -> " + EnvFilePath);
+Console.WriteLine("Default version path -> " + VersionsPath + DefaultVersion + "\\");
+DotEnv.Load(new DotEnvOptions(true, new string[] { EnvFilePath }));
+Console.WriteLine(Environment.GetEnvironmentVariable("GUILD_ID"));
+var procInfo = new ProcessStartInfo();
+procInfo.FileName = VersionsPath + DefaultVersion + "\\DiSH.exe";
+procInfo.WindowStyle = ProcessWindowStyle.Hidden;
+procInfo.CreateNoWindow = true;
+using (Process proc = Process.Start(procInfo))
 {
-    // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-    private static void Main(string[] args)
+    if (proc != null)
     {
-        string AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        string DefaultVersion = Registry.GetValue("HKEY_CURRENT_USER\\LDevs\\DiSHLoader\\", "DefaultVersion", null).ToString();
-        object value = Registry.GetValue("HKEY_CURRENT_USER\\LDevs\\DiSHLoader\\", "EnvPath", AppDataPath + "\\DiSHLoader\\.env");
-        string EnvFilePath = ((value != null) ? value.ToString() : null);
-        string VersionsPath = Registry.GetValue("HKEY_CURRENT_USER\\LDevs\\DiSHLoader\\", "EnvPath", AppDataPath + "\\DiSHLoader\\versions\\").ToString();
-        Console.WriteLine("Default version -> " + DefaultVersion);
-        Console.WriteLine("Env file -> " + EnvFilePath);
-        Console.WriteLine("Default version path -> " + VersionsPath + DefaultVersion + "\\");
-        DotEnv.Load(new DotEnvOptions(true, new string[] { EnvFilePath } ));
-        Console.WriteLine(Environment.GetEnvironmentVariable("GUILD_ID"));
-        var procInfo = new ProcessStartInfo();
-        procInfo.FileName = VersionsPath + DefaultVersion + "\\DiSH.exe";
-        procInfo.WindowStyle = ProcessWindowStyle.Hidden;
-        procInfo.CreateNoWindow = true;
-        using (Process proc = Process.Start(procInfo))
-        {
-            if (proc != null)
-            {
-                proc.WaitForExit();
-            }
-            int exitCode = proc.ExitCode;
-        }
+        proc.WaitForExit();
     }
+    int exitCode = proc.ExitCode;
 }
